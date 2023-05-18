7a5e9c0edabdcbb3b3ed5e426d63c00146480353
asrar <aliasrar84@gmail.com>
2023-05-18 20:05:48 +0530

    new

--- components/Title.tsx
+++ components/Title.tsx
@@ -2,7 +2,7 @@ interface Props {
   titlen:string;
 }
 
-const Title = ({ titlen }) => {
+const Title: React.FunctionComponent<Props> = ({ titlen, }) => {
   return (
     <div><h2 className='m-4 font-titleFont text-2xl font-semibold flex items-center'>
         {titlen} <span className='hidden md:inline-flex md:w-60 lgl:w-72 h-[0.5px] bg-gray-700 ml-6'></span>
