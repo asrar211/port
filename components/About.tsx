dbd6e26a5aca044fbb84fcce1751c4d4c6ab7875
asrar <aliasrar84@gmail.com>
2023-05-18 20:19:37 +0530

    yes

--- components/About.tsx
+++ components/About.tsx
@@ -14,11 +14,11 @@ const About = () => {
             <p>
               Hello! My name is Mohammad Asrar and I am Web Developer and Designer. I have experience in many latest technologies started back from HTML, CSS and JavaScript. I have done various projects which you can see below. Apart from web development, i am currently working on Artficial Intelligence, Machine Learning and Cybersecurity.
               </p>
-              <p>
+              {/* <p>
                 Last Year i had a privilege to work with a startup,
                 <span className='text-textGreen'>called Proton Techs.</span>
-              </p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas quia aut iusto quas adipisci saepe tenetur consequatur eius! Quis, ipsa.</p>
+              </p> */}
+              {/* <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas quia aut iusto quas adipisci saepe tenetur consequatur eius! Quis, ipsa.</p> */}
             <p>Here are some Technologies i have been working with:</p>
             <ul className='max-w-[450px] text-sm font-titleFont grid grid-cols-2 gap-2 mt-6'>
               <li className='flex items-center gap-2'><span className='text-textGreen'><DiJavascript /></span>Javascript</li>
